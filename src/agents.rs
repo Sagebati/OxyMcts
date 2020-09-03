91ed0fc4efcf4d4f431ed655ff665cb005c4e03e
Samuel Batissou <samuelbatissou@gmail.com>
2020-09-03 20:08:27 +0200

    renamed Game into State

--- src/agents.rs
+++ src/agents.rs
@@ -2,7 +2,7 @@ use crate::{DefaultMcts, GameTrait};
 use rand::prelude::SliceRandom;
 use rand::thread_rng;
 
-pub fn mcts_uct_agent<Game: GameTrait>(state: &Game, playouts: usize, c: f64) -> Game::Move {
+pub fn mcts_uct_agent<State: GameTrait>(state: &State, playouts: usize, c: f64) -> State::Move {
     let mut mcts = DefaultMcts::new(state);
     for _ in 0..playouts {
         mcts.execute(&c, ());
@@ -10,7 +10,7 @@ pub fn mcts_uct_agent<Game: GameTrait>(state: &Game, playouts: usize, c: f64) ->
     mcts.best_move(&c)
 }
 
-pub fn random_agent<Game: GameTrait>(state: &Game) -> Game::Move {
+pub fn random_agent<State: GameTrait>(state: &State) -> State::Move {
     state
         .legals_moves()
         .choose(&mut thread_rng())
